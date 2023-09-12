66c7daf6507ded75bd5078a17759c6afc04cf8fb
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-09-12 09:56:14 -0700

    Blade templates
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Task List</h1>
+@extends('layouts.app')
 
+@section('title', 'List of tasks')
+
+@section('content')
     @forelse ($tasks as $task)
         <p>This is task #{{ $task->id }}, and is about: <a href="{{ Route('task.show', ['id' => $task->id]) }}">{{ $task->title }}</a></p>
     @empty
         <h2><strong>No tasks to do maifren</strong></h2>
     @endforelse
-</body>
-</html>
+@endsection
--- task-list/resources/views/show.blade.php
+++ task-list/resources/views/show.blade.php
@@ -1,1 +1,14 @@
-<h1>APARTADO SHOWW </h1>
+@extends('layouts.app')
+
+@section('title', $task->title)
+
+@section('content')
+    <h2>{{ $task -> description }}</h2>
+
+    @if ($task -> long_description)
+        <p>{{$task -> long_description}}</p>
+    @endif
+
+    <p>{{ $task -> created_at }}</p>
+    <p>{{ $task -> updated_at }}</p>
+@endsection
